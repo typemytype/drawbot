d61a5a964eb3e086db1a6c885c6c287da5df4a93
Frederik Berlaen <frederik@typemytype.com>
2013-09-24 16:25:25 +0200

    mock should always create a mock object

--- docs/conf.py
+++ docs/conf.py
@@ -266,10 +266,6 @@ class Mock(object):
     def __getattr__(cls, name):
         if name in ('__file__', '__path__'):
             return '/dev/null'
-        elif name[0] == name[0].upper():
-            mockType = type(name, (), {})
-            mockType.__module__ = __name__
-            return mockType
         else:
             return Mock()
 
