1a5d75e90c67e829c1d82be2664d54c76122dd3f
Frederik Berlaen <frederik@typemytype.com>
2013-09-24 16:15:22 +0200

    adding xmlWriter to mock_moducles

--- docs/conf.py
+++ docs/conf.py
@@ -273,7 +273,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['AppKit', 'Quartz', 'CoreText', 'QTKit']
+MOCK_MODULES = ['AppKit', 'Quartz', 'CoreText', 'QTKit', 'xmlWriter']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
