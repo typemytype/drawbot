ad054888417bdb88bf9e01b96eedc3545847d5ca
Frederik Berlaen <frederik@typemytype.com>
2013-09-30 22:17:20 +0200

    remove slimbox in the docs

--- docs/_themes/drawBotTheme/static/drawBotURLhandler.js
+++ docs/_themes/drawBotTheme/static/drawBotURLhandler.js
@@ -2,11 +2,4 @@ $(document).ready(function () {
 	$(".drawbotlink").each(function () {
 		this.protocol = "drawbot";
 	});
-
-	$('img').wrap(function() {
-    	return $('<a>', {
-        	href: $(this).attr('src'),
-        	rel: 'lightbox-drawbot'
-    	});
-	});
 });
