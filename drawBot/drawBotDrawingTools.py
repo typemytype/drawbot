85763f8e1cabd7f2d0286b16e985f35ab1ca2f14
Frederik Berlaen <frederik@typemytype.com>
2013-11-21 23:42:37 +0100

    fixing missing alpha on image

--- drawBot/drawBotDrawingTools.py
+++ drawBot/drawBotDrawingTools.py
@@ -707,8 +707,11 @@ class DrawBotDrawingTool(object):
         .. showcode:: /../examples/image.py 
         """
         if isinstance(x, (tuple)):
-            alpha = y
+            if alpha is None and y is not None:
+                alpha = y
             x, y = x
+        else:
+            _deprecatedWarning("image(\"%s\", (%s, %s), alpha=%s)" % (path, x, y, alpha))
         if alpha is None:
             alpha = 1
         path = optimizePath(path)
