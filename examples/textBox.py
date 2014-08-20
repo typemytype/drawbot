1893ca789e173a012963be0c927823030c6a48da
Frederik Berlaen <frederik@typemytype.com>
2014-08-20 16:16:00 +0200

    align in text box example

--- examples/textBox.py
+++ examples/textBox.py
@@ -4,6 +4,6 @@ fill(1, 0, 0)
 rect(x, y, w, h)
 fill(1)
 fontSize(50)
-overflow = textBox("hallo, this text is a bit to long", 
-                (x, y, w, h))
+overflow = textBox("hallo, this text is a bit to long",
+                (x, y, w, h), align="center")
 print overflow
