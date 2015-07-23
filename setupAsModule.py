abf36b6a4149b14273209fe90e37a47dd26b244b
Frederik Berlaen <frederik@typemytype.com>
2015-07-23 13:25:52 +0200

    add some useful dependencies warnings

--- setupAsModule.py
+++ setupAsModule.py
@@ -3,6 +3,27 @@ from distutils.core import setup
 
 from drawBotSettings import __version__
 
+try:
+    import fontTools
+except:
+    print "*** Warning: drawBot requires FontTools, see:"
+    print "    https://github.com/behdad/fonttools"
+
+
+try:
+    import vanilla
+except:
+    print "*** Warning: drawBot requires vanilla, see:"
+    print "    https://github.com/typesupply/vanilla"
+
+
+try:
+    import defconAppKit
+except:
+    print "*** Warning: drawBot requires defconAppKit, see:"
+    print "    https://github.com/typesupply/defconAppKit"
+
+
 setup(name="drawBot",
     version=__version__,
     description="DrawBot is a powerful tool that invites you to write simple Python scripts to generate two-dimensional graphics. The builtin graphics primitives support rectangles, ovals, (bezier) paths, polygons, text objects and transparency.",
