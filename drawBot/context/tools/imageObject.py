f002bee9d7237ef58f3500f441e91c43fda88aa8
Frederik Berlaen <frederik@typemytype.com>
2015-12-24 15:35:31 +0100

    add imageObject example

--- drawBot/context/tools/imageObject.py
+++ drawBot/context/tools/imageObject.py
@@ -13,6 +13,8 @@ class ImageObject(object):
 
     For more info see: `Core Image Filter Reference`_.
 
+    .. showcode:: /../../../examples/imageObject.py
+
     .. _Core Image Filter Reference: https://developer.apple.com/library/mac/documentation/GraphicsImaging/Reference/CoreImageFilterReference/index.html
     """
 
