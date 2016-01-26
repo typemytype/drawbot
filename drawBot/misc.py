3c6b16f7db6c202ec4b7a16db8e3f1eb85cb439b
Frederik Berlaen <frederik@typemytype.com>
2016-01-26 11:28:01 +0100

    typo

--- drawBot/misc.py
+++ drawBot/misc.py
@@ -173,7 +173,7 @@ class VariableController(object):
             name = attribute["name"]
             args = dict(attribute.get("args", {}))
             height = 19
-            # addjust the height if a radioGroup is vertical
+            # adjust the height if a radioGroup is vertical
             if uiElement == "RadioGroup":
                 if args.get("isVertical", True):
                     height = height * len(args.get("titles", 1))
