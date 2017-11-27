fdae6be90597c485ae83458849b9ea1a4fce895e
Frederik Berlaen <frederik@typemytype.com>
2017-11-27 16:52:46 +0100

    modify setup to copy the required tools inside the package

--- setupAsModule.py
+++ setupAsModule.py
@@ -1,27 +1,41 @@
 #!/usr/bin/env python
 from setuptools import setup
+import os
+import shutil
 
 from drawBot.drawBotSettings import __version__
 
 try:
     import fontTools
-except:
+except ImportError:
     print "*** Warning: drawBot requires FontTools, see:"
     print "    https://github.com/behdad/fonttools"
 
 
 try:
     import vanilla
-except:
+except ImportError:
     print "*** Warning: drawBot requires vanilla, see:"
     print "    https://github.com/typesupply/vanilla"
 
 
 try:
     import defconAppKit
-except:
+except ImportError:
     print "*** Warning: drawBot requires defconAppKit, see:"
     print "    https://github.com/typesupply/defconAppKit"
+
+
+externalTools = ("ffmpeg", "gifsicle", "mkbitmap", "potrace")
+externalToolsSourceRoot = os.path.join(os.path.dirname(__file__), "Resources", "externalTools")
+externalToolsDestRoot = os.path.join(os.path.dirname(__file__), "drawBot", "context", "tools")
+
+# copy all external tools into drawBot.context.tools  folder
+for externalTool in externalTools:
+    source = os.path.join(externalToolsSourceRoot, externalTool)
+    dest = os.path.join(externalToolsDestRoot, externalTool)
+    shutil.copyfile(source, dest)
+    os.chmod(dest, 0o775)
 
 
 setup(name="drawBot",
@@ -36,14 +50,19 @@ setup(name="drawBot",
         "drawBot.context",
         "drawBot.context.tools",
         "drawBot.ui"
-        ],
-    package_data = {
-        "drawBot" : [
+    ],
+    package_data={
+        "drawBot": [
             "context/tools/ffmpeg",
             "context/tools/gifsicle",
             "context/tools/mkbitmap",
             "context/tools/potrace"
-            ]
-        },
+        ]
+    },
     include_package_data=True,
-    )
+)
+
+# remove all external tools
+for externalTool in externalTools:
+    dest = os.path.join(externalToolsDestRoot, externalTool)
+    os.remove(dest)
