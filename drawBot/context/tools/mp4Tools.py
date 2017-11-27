d8fb03c867c8ae0347abfb8d0fcfa4df6188ec0c
Frederik Berlaen <frederik@typemytype.com>
2017-11-27 16:51:51 +0100

    adding mp4Tools

---
+++ drawBot/context/tools/mp4Tools.py
@@ -0,0 +1,24 @@
+import os
+import AppKit
+
+from drawBot.scriptTools import executeExternalProcess
+
+
+ffmpegPath = os.path.join(os.path.dirname(__file__), "ffmpeg")
+if not os.path.exists(ffmpegPath):
+    ffmpegPath = AppKit.NSBundle.mainBundle().pathForResource_ofType_("ffmpeg", None)
+
+
+def generateMP4(imageTemplate, mp4path, frameRate):
+    cmds = [
+        # ffmpeg path
+        ffmpegPath,
+        "-y",                   # overwrite existing files
+        "-loglevel", "0",       # quiet
+        "-r", str(frameRate),   # frame rate
+        "-i", imageTemplate,    # input sequence
+        "-c:v", "libx264",      # codec
+        "-crf", "20", "-pix_fmt", "yuv420p",  # dunno
+        mp4path,                # output path
+    ]
+    executeExternalProcess(cmds)
