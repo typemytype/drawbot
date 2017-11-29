bed8e06c1fb842f50776747d5255e6c670dd73f4
justvanrossum <justvanrossum@gmail.com>
2017-11-29 09:04:18 +0100

    use different way to make a bitmap image representation, that will not in oddball cases (py+db as odule) use retina/double resolution accidentally

--- drawBot/context/imageContext.py
+++ drawBot/context/imageContext.py
@@ -40,7 +40,7 @@ class ImageContext(PDFContext):
             try:
                 page = pdfDocument.pageAtIndex_(index)
                 image = AppKit.NSImage.alloc().initWithData_(page.dataRepresentation())
-                imageRep = AppKit.NSBitmapImageRep.imageRepWithData_(image.TIFFRepresentation())
+                imageRep = _unscaledBitmapImageRep(image)
                 imageData = imageRep.representationUsingType_properties_(self._saveImageFileTypes[ext], None)
                 imagePath = fileName + pathAdd + fileExt
                 imageData.writeToFile_atomically_(imagePath, True)
@@ -50,3 +50,29 @@ class ImageContext(PDFContext):
             finally:
                 del pool
         return outputPaths
+
+
+def _unscaledBitmapImageRep(image):
+    """Construct a bitmap image representation of 72 DPI, regardless of what kind of display is active."""
+    rep = AppKit.NSBitmapImageRep.alloc().initWithBitmapDataPlanes_pixelsWide_pixelsHigh_bitsPerSample_samplesPerPixel_hasAlpha_isPlanar_colorSpaceName_bytesPerRow_bitsPerPixel_(
+        None,                         # planes
+        int(image.size().width),      # pixelsWide
+        int(image.size().height),     # pixelsHigh
+        8,                            # bitsPerSample
+        4,                            # samplesPerPixel
+        True,                         # hasAlpha
+        False,                        # isPlanar
+        AppKit.NSDeviceRGBColorSpace, # colorSpaceName
+        0,                            # bytesPerRow
+        0                             # bitsPerPixel
+    )
+    rep.setSize_(image.size())
+
+    AppKit.NSGraphicsContext.saveGraphicsState()
+    try:
+        AppKit.NSGraphicsContext.setCurrentContext_(
+            AppKit.NSGraphicsContext.graphicsContextWithBitmapImageRep_(rep))
+        image.drawAtPoint_fromRect_operation_fraction_((0, 0), AppKit.NSZeroRect, AppKit.NSCompositeSourceOver, 1.0)
+    finally:
+        AppKit.NSGraphicsContext.restoreGraphicsState()
+    return rep
