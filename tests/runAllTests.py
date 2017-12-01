88e375d1ff25be5bd1805f5a5e0456320b81de66
justvanrossum <justvanrossum@gmail.com>
2017-12-01 12:57:47 +0100

    add support for doctests; right now there is only one DB module that uses doctests: ddrawBot.misc

--- tests/runAllTests.py
+++ tests/runAllTests.py
@@ -2,6 +2,8 @@ from __future__ import print_function, division, absolute_import
 import sys
 import os
 import unittest
+import doctest
+import importlib
 
 
 testRoot = os.path.dirname(os.path.abspath(__file__))
@@ -9,6 +11,7 @@ if testRoot not in sys.path:
     sys.path.append(testRoot)
 
 files = ["testScripts.py"]  # TODO automatic discovery based on naming convention
+modulesWithDocTests = ["drawBot.misc"]  # TODO: doctest discovery
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
@@ -16,7 +19,10 @@ for fileName in files:
     moduleName, ext = os.path.splitext(fileName)
     suite.addTest(loader.loadTestsFromName(moduleName))
 
-# TODO: add support for doctests
+for moduleName in modulesWithDocTests:
+    m = importlib.import_module(moduleName)
+    suite.addTest(doctest.DocTestSuite(m))
 
+import drawBot.misc as m
 unittest.TextTestRunner(verbosity=1).run(suite)
 # TODO: call sys.exit() with result code if we're not in DB
