8cf64dec3dacd78f1c2a5596ab86f5379a1c4156
justvanrossum <justvanrossum@gmail.com>
2017-12-17 08:19:45 +0100

    renamed folder for temporary test data

--- tests/testSupport.py
+++ tests/testSupport.py
@@ -7,7 +7,7 @@ import random
 
 testRootDir = os.path.dirname(os.path.abspath(__file__))
 testDataDir = os.path.join(testRootDir, "data")
-tempTestDataDir = os.path.join(testRootDir, "temp_data")
+tempTestDataDir = os.path.join(testRootDir, "tempTestData")
 if not os.path.exists(tempTestDataDir):
     os.mkdir(tempTestDataDir)
 
