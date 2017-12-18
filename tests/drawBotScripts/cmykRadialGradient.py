8cf7848fa7d2bb88d98fb276a32d996a524e85b1
justvanrossum <justvanrossum@gmail.com>
2017-12-18 22:31:35 +0100

    The use of uuid is problematic when testing: I didn't find a seed method for those random ids. I replaced it with a deterministinc counter approach per id type

---
+++ tests/drawBotScripts/cmykRadialGradient.py
@@ -0,0 +1,11 @@
+import drawBot
+drawBot.size(200, 200)
+drawBot.cmykRadialGradient(
+    (0, 0),                         # startPoint
+    (200, 200),                         # endPoint
+    [(1, 0, 0, 1), (0, 0, 1, 0), (0, 1, 0, .2)],    # colors
+    [0, .2, 1],                         # locations
+    0,                                  # startRadius
+    200                                 # endRadius
+)
+drawBot.rect(0, 0, 200, 200)
