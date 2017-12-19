436116688d204988174fb37a6fc2244bfec27508
Frederik Berlaen <frederik@typemytype.com>
2017-12-19 15:15:01 +0100

    making the stroke example bigger

--- drawBot/drawBotDrawingTools.py
+++ drawBot/drawBotDrawingTools.py
@@ -779,29 +779,31 @@ class DrawBotDrawingTool(object):
 
             # set the fill to none
             fill(None)
+            # set a stroke width
             stroke(1, 0, 0, .3)
+            strokeWidth(10)
             # draw a rect
-            rect(10, 10, 10, 100)
+            rect(10, 10, 180, 980)
 
             # only set a gray value
             stroke(0)
             # draw a rect
-            rect(30, 10, 10, 100)
+            rect(210, 10, 180, 980)
 
             # only set a gray value with an alpha
             stroke(0, .5)
             # draw a rect
-            rect(50, 10, 10, 100)
+            rect(410, 10, 180, 980)
 
             # set rgb with no alpha
             stroke(1, 0, 0)
             # draw a rect
-            rect(70, 10, 10, 100)
+            rect(610, 10, 180, 980)
 
             # set rgb with an alpha value
             stroke(1, 0, 0, .5)
             # draw a rect
-            rect(90, 10, 10, 100)
+            rect(810, 10, 180, 980)
         """
         self._requiresNewFirstPage = True
         self._addInstruction("stroke", r, g, b, alpha)
