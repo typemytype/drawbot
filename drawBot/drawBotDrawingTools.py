cf703d5a3ab59e97a706739d062daac2235e6f12
Frederik Berlaen <frederik@typemytype.com>
2017-12-19 15:15:22 +0100

    making the radialGradient example bigger

--- drawBot/drawBotDrawingTools.py
+++ drawBot/drawBotDrawingTools.py
@@ -974,15 +974,15 @@ class DrawBotDrawingTool(object):
 
             # set a gradient as the fill color
             radialGradient(
-                (100, 100),                         # startPoint
-                (200, 200),                         # endPoint
+                (300, 300),                         # startPoint
+                (600, 600),                         # endPoint
                 [(1, 0, 0), (0, 0, 1), (0, 1, 0)],  # colors
                 [0, .2, 1],                         # locations
                 0,                                  # startRadius
-                100                                 # endRadius
+                500                                 # endRadius
                 )
             # draw a rectangle
-            rect(100, 100, 100, 100)
+            rect(10, 10, 980, 980)
         """
         self._requiresNewFirstPage = True
         self._addInstruction("radialGradient", startPoint, endPoint, colors, locations, startRadius, endRadius)
