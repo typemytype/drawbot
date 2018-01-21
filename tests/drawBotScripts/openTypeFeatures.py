3b92742614c799666696027da494a84bf1794050
justvanrossum <justvanrossum@gmail.com>
2018-01-21 08:37:56 +0100

    a test for openTypeFeatures()

---
+++ tests/drawBotScripts/openTypeFeatures.py
@@ -0,0 +1,27 @@
+from drawBot import *
+
+size(200, 200)
+
+# ['liga', 'dlig', 'tnum', 'pnum', 'titl', 'onum', 'lnum']
+
+font("HoeflerText-Regular")
+fontSize(20)
+print(listOpenTypeFeatures())
+
+text("Hoefler Fact #123", (20, 170))
+
+openTypeFeatures(None)
+
+openTypeFeatures(dlig=True)
+text("Hoefler Fact #123", (20, 140))
+
+openTypeFeatures(lnum=True)
+text("Hoefler Fact #123", (20, 110))
+
+openTypeFeatures(liga=False)
+text("Hoefler Fact #123", (20, 80))
+
+text("Hoefler Fact #123", (20, 50))
+
+openTypeFeatures(liga=True, resetFeatures=True)
+text("Hoefler Fact #123", (20, 20))
