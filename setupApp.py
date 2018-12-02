e554920eaf3c69b0a7ea3944219fb895cbdfe6dd
Frederik Berlaen <frederik@typemytype.com>
2018-12-02 15:50:38 +0100

    reverse: disable dark mode on 10.14
    all the icons needs to be redrawn...

--- setupApp.py
+++ setupApp.py
@@ -143,7 +143,7 @@ plist = dict(
             CFBundleURLName="com.drawbot",
             CFBundleURLSchemes=[appName.lower()])
     ],
-    NSRequiresAquaSystemAppearance=False,
+    NSRequiresAquaSystemAppearance=True,
 )
 
 
