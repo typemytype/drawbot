ba350524881c28498a1015ac6f5bdbea481a7101
Frederik Berlaen <frederik@typemytype.com>
2018-12-11 23:16:49 +0100

    force path to build and dist when building app inside package that is embedded

--- setupApp.py
+++ setupApp.py
@@ -164,6 +164,10 @@ for fileName in os.listdir("Resources/externalTools"):
     fullPath = os.path.join("Resources/externalTools", fileName)
     dataFiles.append(fullPath)
 
+
+drawBotRoot = os.path.dirname(os.path.abspath(__file__))
+
+
 # build
 setup(
     name=appName,
@@ -171,6 +175,8 @@ setup(
     app=[dict(script="DrawBot.py", plist=plist)],
     options=dict(
         py2app=dict(
+            bdist_base=os.path.join(drawBotRoot, 'build'),
+            dist_dir=os.path.join(drawBotRoot, 'dist'),
             packages=[
                 'vanilla',
                 'defcon',
@@ -218,7 +224,6 @@ writePlist(appPlist, path)
 
 
 # get relevant paths
-drawBotRoot = os.path.dirname(os.path.abspath(__file__))
 distLocation = os.path.join(drawBotRoot, "dist")
 appLocation = os.path.join(distLocation, "%s.app" % appName)
 resourcesPath = os.path.join(appLocation, "Contents", "Resources")
