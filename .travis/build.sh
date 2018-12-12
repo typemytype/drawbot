6eee7f6fe0999778f639c3d548063c85f885f55b
Frederik Berlaen <frederik@typemytype.com>
2018-12-12 18:17:13 +0100

    silly copy paste mistake

--- .travis/build.sh
+++ .travis/build.sh
@@ -12,12 +12,12 @@ python3 -m pip install -r ./test-requirements.txt
 
 # install dev certificate
 export CERTIFICATE_P12=Certificate.p12
-echo $CERTIFICATE_OSX_P12 | base64 — decode > $CERTIFICATE_P12
+echo $CERTIFICATE_OSX_P12 | base64 --decode > $CERTIFICATE_P12
 export KEYCHAIN=build.keychain
 security create-keychain -p mysecretpassword $KEYCHAIN
-security default-keychain -s $KEYCHAIN;
-security unlock-keychain -p mysecretpassword $KEYCHAIN;
-security import $CERTIFICATE_P12 -k $KEYCHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign;
+security default-keychain -s $KEYCHAIN
+security unlock-keychain -p mysecretpassword $KEYCHAIN
+security import $CERTIFICATE_P12 -k $KEYCHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign
 
 # make sure drawBot is not installed as module
 # py2app gets confused when building the app inside the package
