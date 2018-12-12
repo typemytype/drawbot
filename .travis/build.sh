218928ceded55e6d62f6e5b0fc87c7325f9e9283
Frederik Berlaen <frederik@typemytype.com>
2018-12-12 18:24:50 +0100

    dont unisntall drawbot

--- .travis/build.sh
+++ .travis/build.sh
@@ -19,8 +19,5 @@ security default-keychain -s $KEYCHAIN
 security unlock-keychain -p mysecretpassword $KEYCHAIN
 security import $CERTIFICATE_P12 -k $KEYCHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign
 
-# make sure drawBot is not installed as module
-# py2app gets confused when building the app inside the package
-python3 -m pip uninstall --yes drawBot
 # build the app
 python3 ./setupApp.py py2app --dmg --codesign "Frederik Berlaen"
