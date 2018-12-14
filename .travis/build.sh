abd171e518ecbc828863dece305745b770012e2d
Frederik Berlaen <frederik@typemytype.com>
2018-12-14 16:49:17 +0100

    adding notes and turn on test.sh

--- .travis/build.sh
+++ .travis/build.sh
@@ -11,6 +11,8 @@ python3 -m pip install -r ./requirements.txt
 python3 -m pip install -r ./test-requirements.txt
 
 # install dev certificate
+# https://github.com/danielbuechele/atemOSC/blob/864f95931a7f6ae35a81542b2ca7157124166693/.travis/sign.sh
+# https://www.jviotti.com/2016/03/16/how-to-code-sign-os-x-electron-apps-in-travis-ci.html
 export CERTIFICATE_P12=Certificate.p12
 echo $CERTIFICATE_OSX_P12 | base64 --decode > $CERTIFICATE_P12
 export KEYCHAIN=build.keychain
