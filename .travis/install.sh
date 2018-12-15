af747f180e50def279475ac58a3b9bee3be3d1cb
Frederik Berlaen <frederik@typemytype.com>
2018-12-15 14:05:40 +0100

    updating travis settings

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,8 +5,8 @@ set -x
 
 # install current python3 with homebrew
 # NOTE: the formula is now named just "python"
-# brew update
-# brew upgrade python
+brew update
+brew upgrade python
 command -v python3
 python3 --version
 python3 -m pip install virtualenv
