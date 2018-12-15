5131b8430d82ca85c7b52f557db846f47c0ce086
Frederik Berlaen <frederik@typemytype.com>
2018-12-15 16:14:17 +0100

    install python for 10.10

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,7 +6,13 @@ set -x
 # install current python3 with homebrew
 # NOTE: the formula is now named just "python"
 brew update
-brew upgrade python
+if [[ $TRAVIS_OSX_IMAGE == 'xcode6.4' ]];
+then
+    brew install python
+else
+    brew upgrade python
+fi
+
 command -v python3
 python3 --version
 python3 -m pip install virtualenv
