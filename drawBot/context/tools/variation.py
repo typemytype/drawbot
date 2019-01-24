153258ae50a0c0ea5c4ee88c3a6e5f427b09fc95
Frederik Berlaen <frederik@typemytype.com>
2019-01-24 11:03:13 +0100

    adding support for getting named instances

--- drawBot/context/tools/variation.py
+++ drawBot/context/tools/variation.py
@@ -1,6 +1,8 @@
 import CoreText
 from collections import OrderedDict
 
+from fontTools.ttLib import TTFont
+
 from drawBot.misc import memoize
 
 """
@@ -42,3 +44,45 @@ def getVariationAxesForFontName(fontName):
         data = dict(name=name, minValue=minValue, maxValue=maxValue, defaultValue=defaultValue)
         axes[tag] = data
     return axes
+
+
+@memoize
+def getVariationNamedInstancesForFontName(fontName):
+    """
+    Return a dict { postscriptName: location } of all named instances in a given font.
+    """
+    instances = {}
+    font = CoreText.CTFontCreateWithName(fontName, 12, None)
+    if font is None:
+        return instances
+    cgFont = CoreText.CGFontCreateWithFontName(fontName)
+    fontDescriptor = font.fontDescriptor()
+    url = CoreText.CTFontDescriptorCopyAttribute(fontDescriptor, CoreText.kCTFontURLAttribute)
+    if url is None:
+        return instances
+
+    variationAxesDescriptions = CoreText.CTFontCopyVariationAxes(font)
+    if variationAxesDescriptions is None:
+        # 'normal' fonts have no named instances
+        return instances
+    tagNameMap = {}
+    for variationAxesDescription in variationAxesDescriptions:
+        tag = convertIntToVariationTag(variationAxesDescription[CoreText.kCTFontVariationAxisIdentifierKey])
+        name = variationAxesDescription[CoreText.kCTFontVariationAxisNameKey]
+        tagNameMap[tag] = name
+
+    ft = TTFont(url.path())
+    if "fvar" in ft:
+        fvar = ft["fvar"]
+
+        for instance in fvar.instances:
+            fontVariations = dict()
+            for axis, value in instance.coordinates.items():
+                fontVariations[tagNameMap[axis]] = value
+
+            varFont = CoreText.CGFontCreateCopyWithVariations(cgFont, fontVariations)
+            postScriptName = CoreText.CGFontCopyPostScriptName(varFont)
+            instances[postScriptName] = instance.coordinates
+
+    ft.close()
+    return instances
