0c11a06dc5c41c4c22b918a18d9d4bc26b171b1b
Frederik Berlaen <frederik@typemytype.com>
2019-10-30 22:13:10 +0100

    remove the S
     its a  var font sub rule that is not applied on older macOSs

--- tests/drawBotScripts/fontPath.py
+++ tests/drawBotScripts/fontPath.py
@@ -3,6 +3,6 @@ drawBot.size(500, 70)
 fontPath = "../data/MutatorSans.ttf"
 drawBot.font(fontPath)
 drawBot.fontSize(20)
-drawBot.text("HELLO MUTATOR SANS", (10, 10))
+drawBot.text("HELLO MUTATOR", (10, 10))
 drawBot.fontVariations(wdth=500, wght=900)
-drawBot.text("HELLO MUTATOR SANS", (10, 40))
+drawBot.text("HELLO MUTATOR", (10, 40))
