7f3e3d45ea704e870fa64de0822807a6edc7e8bb
justvanrossum <justvanrossum@gmail.com>
2019-11-02 13:35:49 +0100

    Use popup button to select pip command, added (default) 'Go' button to trigger pip; moved extra options to separate action button; allow multiple package names to be entered (and even additional command line options)

--- drawBot/pipInstaller.py
+++ drawBot/pipInstaller.py
@@ -1,5 +1,7 @@
 import os
+import re
 import select
+import shlex
 import subprocess
 import sys
 import threading
@@ -10,23 +12,12 @@ from vanilla.dialogs import message
 from drawBot.ui.codeEditor import OutPutEditor as OutputEditor
 
 
-class MyActionButton(ActionButton):
+welcomeText = """\
+Welcome to pip! Pip installs Python Packages from the PyPI database.
 
-    def __init__(self, posSize, items, *args, **kwargs):
-        super().__init__(posSize, items, *args, **kwargs)
-        self._nsObject.setAutoenablesItems_(False)
-        self.menuItemIndices = {item["name"]: index + 1 for index, item in enumerate(items)}
-        separatorIndices = [index + 1 for index, item in enumerate(items) if not item["title"]]
-        menu = self._nsObject.menu()
-        for index in separatorIndices:
-            item = AppKit.NSMenuItem.separatorItem()
-            menu.removeItemAtIndex_(index)
-            menu.insertItem_atIndex_(item, index)
-
-    def enableItem(self, name, onoff):
-        item = self._nsObject.itemAtIndex_(self.menuItemIndices[name])
-        item.setEnabled_(onoff)
-
+Enter one or more package names in the text field, choose an action from \
+the popup button on the left, then click “Go!” or hit return or enter.
+"""
 
 class PipInstallerController:
 
@@ -35,33 +26,36 @@ class PipInstallerController:
         self._isRunning = False
 
         y = 15
-        self.w = Window((640, 300), "Install External Python Packages", 
+        self.w = Window((640, 300), "Install Python Packages", 
                 minSize=(640, 300), autosaveName="PipInstaller")
+
+        items = ["Search PyPI", "Install / Upgrade", "Uninstall", "Show Package Info"]
+        self.pipCommandNames = [f"pip{re.sub(r'[ /]', '', item)}Command" for item in items]
+        self.w.pipCommandsButton = PopUpButton((15, y, 140, 25), items)
+        self.w.pipCommandsButton.getNSPopUpButton().setBezelStyle_(AppKit.NSBezelStyleTexturedRounded)        
+        self.w.textEntry = EditText((170, y, -175, 25), placeholder="Enter one or more package names",
+                callback=self.textEntryCallback)
+        self.w.goButton = Button((-160, y, 50, 25), "Go!", callback=self.goButtonCallback)
+        self.w.goButton.enable(False)
+        self.w.setDefaultButton(self.w.goButton)
+        self.w.progressSpinner = ProgressSpinner((-100, y, 25, 25))
+
         items = [
-            dict(name="search", title="Search PyPI", callback=self.pipSearchCallback),
-            dict(name="install", title="Install / Upgrade", callback=self.pipInstallCallback),
-            dict(name="uninstall", title="Uninstall", callback=self.pipUninstallCallback),
-            dict(name="showInfo", title="Show Package Info", callback=self.pipShowCallback),
-            dict(name="_separator_", title=""),
             dict(name="list", title="List Installed Packages", callback=self.pipListCallback),
-            dict(name="showInstallFolder", title="Show Install Folder", callback=self.showInstallFolderCallback),
             dict(name="showPipVersion", title="Show Pip Version", callback=self.pipVersionCallback),
+            "----",
+            dict(name="revealInstallFolder", title="Reveal Install Folder in Finder", callback=self.revealInstallFolderCallback),
         ]
-        self.w.actionButton = MyActionButton((15, y, 40, 25), items)
-        self.w.textEntry = EditText((70, y, -55, 25), placeholder="Enter a package name",
-                callback=self.textEntryChanged)
-        self.w.progressSpinner = ProgressSpinner((-40, y, 25, 25))
+        self.w.extraActionButton = ActionButton((-60, y, 40, 25), items)
         y += 35
 
         self.w.outputField = OutputEditor((0, y, -0, -20), readOnly=True)
         self.w.resultcodeField = TextBox((10, -18, 200, 0), "", sizeStyle="small")
 
-        self.textEntryChanged(None)  # set menu items to initial state
         self.w.open()
 
         self.w.bind("should close", self.windowShouldClose)
-        self.stdoutCallback("Welcome to pip! Pip installs Python Packages from the PyPI database.\n")
-        self.stdoutCallback("Enter a package name in the text field, then choose an action from the “gear” button.\n")
+        self.stdoutCallback(welcomeText)
 
     def show(self):
         if self.w._window is None:
@@ -84,47 +78,43 @@ class PipInstallerController:
     @isRunning.setter
     def isRunning(self, onoff):
         self._isRunning = onoff
-        self.enableActionMenuItems(not onoff)
         if onoff:
             self.w.progressSpinner.start()
         else:
             self.w.progressSpinner.stop()
-        self.w.actionButton.enableItem("showInstallFolder", os.path.exists(self.targetPath))
+        self.w.goButton.enable(not onoff)
 
-    @property
-    def enteredText(self):
-        return self.w.textEntry.get().strip()
+    def getUserArguments(self):
+        return shlex.split(self.w.textEntry.get())
 
-    def enableActionMenuItems(self, onoff):
-        for menuName in ["list", "showPipVersion"]:
-            self.w.actionButton.enableItem(menuName, onoff)
-        if not self.enteredText:
-            onoff = False
-        for menuName in ["search", "install", "showInfo", "uninstall"]:
-            self.w.actionButton.enableItem(menuName, onoff)
+    def goButtonCallback(self, sender):
+        commandName = self.pipCommandNames[self.w.pipCommandsButton.get()]
+        getattr(self, commandName)(self.getUserArguments())
 
-    def textEntryChanged(self, sender):
+    def textEntryCallback(self, sender):
         if self.isRunning:
             return
-        self.enableActionMenuItems(not self.isRunning)
+        self.w.goButton.enable(bool(self.getUserArguments()))
 
-    def pipSearchCallback(self, sender):
-        self.callPip(["search", self.enteredText])
+    def pipSearchPyPICommand(self, userArguments):
+        self.callPip(["search"] + userArguments)
 
-    def pipInstallCallback(self, sender):
+    def pipInstallUpgradeCommand(self, userArguments):
         if not os.path.exists(self.targetPath):
             os.makedirs(self.targetPath)
             if self.targetPath not in sys.path:
                 sys.path.append(self.targetPath)
-        self.callPip(["install", "--upgrade", "--target", self.targetPath, self.enteredText])
+        self.callPip(["install", "--upgrade", "--target", self.targetPath] + userArguments)
 
-    def pipShowCallback(self, sender):
-        self.callPip(["show", self.enteredText])
+    def pipShowPackageInfoCommand(self, userArguments):
+        self.callPip(["show"] + userArguments)
 
     def pipListCallback(self, sender):
-        self.callPip(["list", self.enteredText])
+        self.callPip(["list"])
 
-    def pipUninstallCallback(self, sender):
+    def pipUninstallCommand(self, userArguments):
+        packageNames = [arg.lower() for arg in userArguments if not arg.startswith("-")]
+        extraArguments = [arg for arg in userArguments if arg.startswith("-")]
         outputLines = []
         def collectOutput(data):
             outputLines.append(data)
@@ -133,31 +123,45 @@ class PipInstallerController:
                 self.stderrCallback("".join(outputLines))
                 self.isRunning = False
                 return
-            tag = "Location:"
-            locationLines = [line for line in outputLines if line.startswith(tag)]
-            if not locationLines:
-                self.stderrCallback("".join(outputLines))
-                self.isRunning = False
-                return
-            locationLine = locationLines[0][len(tag):].strip()
-            if locationLine != self.targetPath:
-                self.stderrCallback(f"Can't uninstall: {self.enteredText} is not installed in {self.targetPath}\n")
-                self.isRunning = False
+            packages = {}
+            nameTag = "Name:"
+            locationTag = "Location:"
+            for line in outputLines:
+                if line.startswith(nameTag):
+                    name = line[len(nameTag):].strip()
+                elif line.startswith(locationTag):
+                    location = line[len(locationTag):].strip()
+                    assert name is not None
+                    packages[name.lower()] = location
+                    name = None
+            packageNamesNotFound = {name for name in packageNames if name not in packages}
+            packageNamesBad = [name for name in packageNames if name in packages and packages[name] != self.targetPath]
+            packageNamesGood = [name for name in packageNames if name in packages and packages[name] == self.targetPath]
+            for name in packageNamesNotFound:
+                self.stderrCallback(f"Skipping {name} as it is not installed\n")
+            for name in packageNamesBad:
+                self.stderrCallback(f"Skipping {name} as it is not installed in {self.targetPath}\n")
+            if packageNamesGood:
+                self.callPip(["uninstall", "-y"] + extraArguments + packageNamesGood, clearOutput=False)
             else:
-                self.callPip(["uninstall", self.enteredText, "-y"])
+                self.isRunning = False
         self.w.outputField.clear()
         self.isRunning = True
         self.setResultCode("--")
-        callPip(["show", self.enteredText], collectOutput, collectOutput, doneShowCallback)
+        callPip(["show"] + packageNames, collectOutput, collectOutput, doneShowCallback)
 
     def pipVersionCallback(self, sender):
         self.callPip(["--version"])
 
-    def showInstallFolderCallback(self, sender):
-        AppKit.NSWorkspace.sharedWorkspace().selectFile_inFileViewerRootedAtPath_(None, self.targetPath)
+    def revealInstallFolderCallback(self, sender):
+        if not os.path.exists(self.targetPath):
+            message("The install folder does not yet exists.", "Try again after installing a package.", parentWindow=self.w)
+        else:
+            AppKit.NSWorkspace.sharedWorkspace().selectFile_inFileViewerRootedAtPath_(None, self.targetPath)
 
-    def callPip(self, arguments):
-        self.w.outputField.clear()
+    def callPip(self, arguments, clearOutput=True):
+        if clearOutput:
+            self.w.outputField.clear()
         self.isRunning = True
         self.setResultCode("--")
         callPip(arguments, self.stdoutCallback, self.stderrCallback, self.resultCallback)
@@ -173,6 +177,8 @@ class PipInstallerController:
     def resultCallback(self, resultcode):
         self.setResultCode(resultcode)
         self.isRunning = False
+        if resultcode == 23:  # special pip error code
+            self.stdoutCallback("No results.\n")
 
     def setResultCode(self, resultcode):
         self.w.resultcodeField.set(f"pip result code: {resultcode}")
