00ac412f949571b3270797cf0e74c8c773e20641
justvanrossum <justvanrossum@gmail.com>
2019-11-03 12:39:20 +0100

    no need to bundle uharfbuzz now that we have integrated pip

--- setupApp.py
+++ setupApp.py
@@ -186,7 +186,6 @@ setup(
                 'ufo2svg',
                 'fontPens',
                 'booleanOperations',
-                'uharfbuzz',
                 # 'pyclipper',
                 'pygments',
                 'jedi',
