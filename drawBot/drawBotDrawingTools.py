02e2283f2de1e6ca3484e3cfcc005873efb34d42
Frederik Berlaen <frederik@typemytype.com>
2020-04-13 12:05:06 +0200

    improve doc

--- drawBot/drawBotDrawingTools.py
+++ drawBot/drawBotDrawingTools.py
@@ -1796,7 +1796,7 @@ class DrawBotDrawingTool(object):
 
     def textBoxCharacterBounds(self, txt, box, align=None):
         """
-        Returns a list of `((x, y, w, h), baseLineOffset, characters, formattedString)`.
+        Returns a list of typesetted bounding boxes `((x, y, w, h), baseLineOffset, characters, formattedString)`.
 
         A `box` could be a `(x, y, w, h)` or a bezierPath object.
 
