01919e5a9212501b842a50c95d15a8d25cb3f762
Frederik Berlaen <frederik@typemytype.com>
2023-03-10 17:10:13 +0100

    check language

--- drawBot/misc.py
+++ drawBot/misc.py
@@ -197,6 +197,25 @@ def nsStringLength(s):
     return len(s.encode("utf-16-be")) // 2
 
 
+# ===================
+# = language tools  =
+# ===================
+
+def canonicalLocaleCode(localeCode):
+    parsedLoc = AppKit.NSLocale.componentsFromLocaleIdentifier_(localeCode)
+    parts = [
+        parsedLoc[AppKit.kCFLocaleLanguageCode],
+        parsedLoc.get(AppKit.kCFLocaleScriptCode),
+        parsedLoc.get(AppKit.kCFLocaleCountryCode),
+    ]
+    return "_".join(part for part in parts if part)
+
+
+def validateLanguageCode(localeCode):
+    localeCode = canonicalLocaleCode(localeCode)
+    return localeCode in AppKit.NSLocale.availableLocaleIdentifiers()
+
+
 # ============
 # = warnings =
 # ============
