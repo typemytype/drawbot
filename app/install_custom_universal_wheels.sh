61464fdd1a2e569d94e74aa4737164700e2be85c
Just van Rossum <justvanrossum@gmail.com>
2024-01-28 12:37:25 +0100

    Let's try to pin Pillow to 10.1.0

--- app/install_custom_universal_wheels.sh
+++ app/install_custom_universal_wheels.sh
@@ -4,8 +4,9 @@ set -e # abort on errors
 set -x # echo commands
 
 
-for package in pillow
+for package_version in pillow==10.1.0
 do
-	python app/build_universal_wheel.py $package
+	package=(${package_version//==/ })
+	python app/build_universal_wheel.py $package_version
 	pip install --force build/universal_wheels/$package*.whl
 done
