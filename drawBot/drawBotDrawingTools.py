1a40dc13830ef4a7f34063bf15596beb411ec542
Roberto Arista <arista.rob@gmail.com>
2024-05-15 14:56:53 +0200

    update image url

--- drawBot/drawBotDrawingTools.py
+++ drawBot/drawBotDrawingTools.py
@@ -2082,8 +2082,8 @@ class DrawBotDrawingTool():
 
         .. downloadcode:: image.py
 
-            # the path can be a path to a file or a url
-            image("https://d1sz9tkli0lfjq.cloudfront.net/items/1T3x1y372J371p0v1F2Z/drawBot.jpg", (100, 100), alpha=.3)
+            # the path can be a path to a file or a url, image from https://www.wired.com/2012/08/a1-art-0-99-vending-machines-as-art-galleries/
+            image("https://raw.githubusercontent.com/typemytype/drawbot/master/tests/data/drawBot.jpg", (100, 100), alpha=.3)
         """
         if isinstance(path, self._imageClass):
             path = path._nsImage()
@@ -2102,7 +2102,7 @@ class DrawBotDrawingTool():
 
         .. downloadcode:: imageSize.py
 
-            print(imageSize("https://d1sz9tkli0lfjq.cloudfront.net/items/1T3x1y372J371p0v1F2Z/drawBot.jpg"))
+            print(imageSize("https://raw.githubusercontent.com/typemytype/drawbot/master/tests/data/drawBot.jpg"))
         """
         if isinstance(path, self._imageClass):
             # its an drawBot.ImageObject, just return the size from that obj
@@ -2163,8 +2163,8 @@ class DrawBotDrawingTool():
 
         .. downloadcode:: pixelColor.py
 
-            # path to the image
-            path = u"https://d1sz9tkli0lfjq.cloudfront.net/items/1T3x1y372J371p0v1F2Z/drawBot.jpg"
+            # path to the image, from https://www.wired.com/2012/08/a1-art-0-99-vending-machines-as-art-galleries/
+            path = "https://raw.githubusercontent.com/typemytype/drawbot/master/tests/data/drawBot.jpg"
 
             # get the size of the image
             w, h = imageSize(path)
