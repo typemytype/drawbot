b3bc4777f1b748bfbcccf8c0ffad462379f58fb8
Roberto Arista <arista.rob@gmail.com>
2024-07-29 20:38:18 +0200

    fix annotation

--- drawBot/context/baseContext.py
+++ drawBot/context/baseContext.py
@@ -2322,8 +2322,8 @@ class BaseContext:
     _bezierPathClass = BezierPath
     _gradientClass = Gradient
 
-    fileExtensions: list[str] = []
-    saveImageOptions: list[str] = []
+    fileExtensions: list[tuple[str, str]] = []
+    saveImageOptions: list[tuple[str, str]] = []
     validateSaveImageOptions = True
 
     _textAlignMap = FormattedString._textAlignMap
