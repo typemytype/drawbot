8ba36f306291e62d7ddfadf1be16a2a862335bfe
Roberto Arista <arista.rob@gmail.com>
2026-05-19 09:08:45 +0200

    restore print block

--- tests/drawBotScripts/fontVariations.py
+++ tests/drawBotScripts/fontVariations.py
@@ -6,8 +6,15 @@ drawBot.font("Skia")
 # pick a font size
 drawBot.fontSize(200)
 # list all axis from the current font
-for axis, data in drawBot.listFontVariations().items():
-    print((axis, data))
+variations = drawBot.listFontVariations()
+for axisTag in sorted(variations):
+    data = variations[axisTag]
+    # round values to avoid small per-OS float differences
+    data["defaultValue"] = round(float(data["defaultValue"]), 3)
+    data["minValue"] = round(float(data["minValue"]), 3)
+    data["maxValue"] = round(float(data["maxValue"]), 3)
+    data["name"] = str(data["name"])
+    print(axisTag, [(k, data[k]) for k in sorted(data)])
 # pick a variation from the current font
 drawBot.fontVariations(wght=0.6)
 # draw text!!
